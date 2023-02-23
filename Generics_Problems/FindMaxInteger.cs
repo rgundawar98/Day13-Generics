b65b6b432d1fa15a7fa957c090be244ec7f99e0c
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-23 21:10:10 +0530

    Find maximm integer number from three integers UC1_FindMaxIntegerNumber

---
+++ Generics_Problems/FindMaxInteger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Problems
+{
+    public class FindMaxInteger
+    {
+        public void Find_Maximum_Integer(int firstval , int secondval , int thirdval)
+        {
+            if(firstval.CompareTo(secondval) > 0 && firstval.CompareTo(thirdval)>0 )
+            {
+                Console.WriteLine("{0} is maximum number", firstval);
+            }
+            else if(secondval.CompareTo(firstval)>0 && secondval.CompareTo(thirdval)>0)
+            {
+                Console.WriteLine("{0} is maximum number", secondval);
+            }
+            else
+            {
+                Console.WriteLine("{0} is maximum number", thirdval);
+            }
+        }
+    }
+}
--- Generics_Problems/Program.cs
+++ Generics_Problems/Program.cs
@@ -7,6 +7,22 @@ namespace Generics_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generics Problems");
+            Console.WriteLine("1.Find Max Integer");
+            Console.WriteLine("Choose option from above:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the first value");
+                    int firstval = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the second value");
+                    int secondval = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the third value");
+                    int thirdval = Convert.ToInt32(Console.ReadLine());
+                    FindMaxInteger findMaxInteger= new FindMaxInteger();
+                    findMaxInteger.Find_Maximum_Integer(firstval, secondval, thirdval);
+                    break;
+            }
         }
     }
 }
