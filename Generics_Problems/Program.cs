d91dcf6fd2b94d557383ba61bb1e7959471ab842
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-23 20:41:27 +0530

    Adding welcome message

---
+++ Generics_Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generics_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generics Problems");
+        }
+    }
+}
