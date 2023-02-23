bccbdb52efd351d8a47b952da7bb2eba3ad64686
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-23 21:21:44 +0530

    Find maximun float number from three float numbers UC2_FindMaxFloatNumber

---
+++ Generics_Problems/FindMaxFloat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Problems
+{
+    public class FindMaxFloat
+    {
+        public void Find_Maximum_Float(double first, double second, double third) 
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is maximum number", first);
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                Console.WriteLine("{0} is maximum number", second);
+            }
+            else
+            {
+                Console.WriteLine("{0} is maximum number", third);
+            }
+        }
+    }
+}
--- Generics_Problems/Program.cs
+++ Generics_Problems/Program.cs
@@ -7,7 +7,7 @@ namespace Generics_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generics Problems");
-            Console.WriteLine("1.Find Max Integer");
+            Console.WriteLine("1.Find Max Integer\n2.Find Max Float or Double");
             Console.WriteLine("Choose option from above:");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -21,6 +21,16 @@ namespace Generics_Problems
                     int thirdval = Convert.ToInt32(Console.ReadLine());
                     FindMaxInteger findMaxInteger= new FindMaxInteger();
                     findMaxInteger.Find_Maximum_Integer(firstval, secondval, thirdval);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the first number");
+                    double first = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter the second number");
+                    double second = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Enter the third number");
+                    double third = Convert.ToDouble(Console.ReadLine());
+                    FindMaxFloat findMaxFloat= new FindMaxFloat();
+                    findMaxFloat.Find_Maximum_Float(first, second, third);
                     break;
             }
         }
