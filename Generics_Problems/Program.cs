ec69279f1f6fc15e62a4144df6ade485df82ea19
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-24 20:08:19 +0530

    Combine all three methods into one using generic method to print maximum integer,double,string UC4_RefactorThreeMethods

--- Generics_Problems/Program.cs
+++ Generics_Problems/Program.cs
@@ -7,7 +7,7 @@ namespace Generics_Problems
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generics Problems");
-            Console.WriteLine("1.Find Max Integer\n2.Find Max Float or Double\n3.Find Maximum String");
+            Console.WriteLine("1.Find Max Integer\n2.Find Max Float or Double\n3.Find Maximum String\n4.Generic Refactor one");
             Console.WriteLine("Choose option from above:");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -41,6 +41,11 @@ namespace Generics_Problems
                     string Third = Convert.ToString(Console.ReadLine());
                     FindMaxString findMaxString= new FindMaxString();
                     findMaxString.Find_Maximum_String(First, Second, Third);
+                    break;
+                case 4:
+                    GenericRefactor<int>.FindMax(120, 222, 333);
+                    GenericRefactor<double>.FindMax(45.6, 55.8, 20.5);
+                    GenericRefactor<string>.FindMax("Apple", "Banana", "Pineapple");
                     break;
             }
         }
---
+++ Generics_Problems/RefactorOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Problems
+{
+    public class GenericRefactor<T> where T :IComparable
+    {
+        public static T FindMax(T First_val , T Second_Val, T Third_Val)
+        {
+            if(First_val.CompareTo(Second_Val)>0 && First_val.CompareTo(Third_Val)>0)
+            {
+                Console.WriteLine("Greater is:"+First_val);
+                return First_val;
+            }
+            else if(Second_Val.CompareTo(Third_Val)>0 && Second_Val.CompareTo(First_val)>0)
+            {
+                Console.WriteLine("Greater is:" + Second_Val);
+                return Second_Val;
+            }
+            else
+            {
+                Console.WriteLine("Greater is:" + Third_Val);
+                return Third_Val;
+            }
+        }
+    }
+}
